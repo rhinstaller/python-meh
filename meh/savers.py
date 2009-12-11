5f1acbc4f8b1c0da16935ba7ecfcc7dd8506b228
Chris Lumens <clumens@redhat.com>
2009-12-11 11:23:06 -0500

    Credit where credit is due.

--- meh/savers.py
+++ meh/savers.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Author(s): Chris Lumens <clumens@redhat.com>
+#            Will Woods <wwoods@redhat.com>
 #
 from filer import *
 import os
