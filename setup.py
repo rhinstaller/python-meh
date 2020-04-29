63267f01fe1fc404a5efca5e51470af598493fc3
Vladimir Slavik <vslavik@redhat.com>
2020-04-29 16:51:34 +0200

    Fix a large part of setup.py
    Fixes: dependencies, author (maintainer) + email, link.

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@
 
 from distutils.core import setup
 
-setup(name='python-meh', version='0.48',
+setup(name='python-meh',
+      version='0.48',
       description='Python module for handling exceptions',
-      author='Chris Lumens', author_email='clumens@redhat.com',
-      url='http://git.fedoraproject.org/git/?p=python-meh.git',
-      data_files = [('/usr/share/python-meh', ['ui/exception-dialog.glade'])],
-      packages=['meh', 'meh.ui'])
+      license="GPL-2.0+",
+      author='Chris Lumens',
+      author_email='clumens@redhat.com',
+      maintainer='Anaconda maintenance team',
+      maintainer_email='anaconda-maint-list@redhat.com',
+      url='https://github.com/rhinstaller/python-meh',
+      data_files=[('/usr/share/python-meh', ['ui/exception-dialog.glade'])],
+      packages=['meh', 'meh.ui'],
+      install_requires=['dbus-python', 'PyGObject', 'report', 'rpm'])
